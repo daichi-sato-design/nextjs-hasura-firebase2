23d8bc03dfb4eedeae8cbb91c953ad3623976445
daichi-sato-design <daichisato.design@gmail.com>
2021-10-02 23:18:03 +0900

    Logout時にCacheとreduxの削除を実装

--- hooks/useLogout.ts
+++ hooks/useLogout.ts
@@ -1,15 +1,25 @@
 import Cookie from 'universal-cookie'
 import firebase from '../firebaseConfig'
 import { unSubMeta } from './useUserChanged'
+import { useQueryClient } from 'react-query'
+import { useDispatch } from 'react-redux'
+import { resetEditedTask, resetEditedNews } from '../slices/uiSlice'
 
 const cookie = new Cookie()
 
 export const useLogout = () => {
+  const dispatch = useDispatch()
+  const queryClient = useQueryClient()
+
   const logout = async () => {
     if (unSubMeta) {
       unSubMeta()
     }
     await firebase.auth().signOut()
+    dispatch(resetEditedTask())
+    dispatch(resetEditedNews())
+    queryClient.resetQueries('tasks')
+    queryClient.resetQueries('news')
     cookie.remove('token')
   }
 
